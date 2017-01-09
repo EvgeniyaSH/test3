1506813caa656f446484062b770e9c08b5c0c777
EvgeniaSH <migaliss@gmail.com>
2017-01-09 23:20:30 +0300

    trying 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Let's play a game!");
+        System.out.println("Let's play a game! You have to gess the number from 1 to 15");
     }
 }
